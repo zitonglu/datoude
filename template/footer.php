dbc18d85c59503c0b301c67a9ef4a70185afa31e
zitonglu <910109610@qq.com>
2017-09-08 22:02:59 +0800

    add footer.php

---
+++ template/footer.php
@@ -0,0 +1,8 @@
+<?php echo'<meta charset="UTF-8"><div style="text-align:center;padding:60px 0;font-size:16px;">
+        <h2 style="font-size:60px;margin-bottom:32px;color:f00;">主题由<a href="http://limiwu.com">紫铜炉</a>设计制作</h2>
+</div>';die();?>
+
+
+{$footer}
+</body>
+</html>
