b2449d7c6eb0ea43944b055391d5f4fffdda13ba
zitonglu <910109610@qq.com>
2017-09-14 22:47:07 +0800

    footer.php

--- template/footer.php
+++ template/footer.php
@@ -2,7 +2,13 @@
         <h2 style="font-size:60px;margin-bottom:32px;color:f00;">主题由<a href="http://limiwu.com">紫铜炉</a>设计制作</h2>
 </div>';die();?>
 
-
+<footer>
+    <div class="footerNav container">
+        <ul class="list-inline">
+            {module:navbar}
+        </ul>
+    </div>
+</footer>
 {$footer}
 </body>
 </html>
