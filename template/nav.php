ee4fc79db0562ea9f1f36f8f6c5ce0e8c13d8cac
zitonglu <910109610@qq.com>
2017-10-02 10:56:15 +0800

    nav

--- template/nav.php
+++ template/nav.php
@@ -1,6 +1,17 @@
 <?php echo'<meta charset="UTF-8"><div style="text-align:center;padding:60px 0;font-size:16px;">
         <h2 style="font-size:60px;margin-bottom:32px;color:f00;">主题由<a href="http://www.paipk.com">紫铜炉</a>设计制作</h2>
 </div>';die();?>
+<div class="container logo">
+	<div class="col-sm-7 ComLogo">
+		<a href="{$host}" title="{$name}"><p><b>南通恒宇科学仪器有限公司</b><br><span>NanTongHengYu Scientific Instrument Co Ltd</span></p></a>
+	</div>
+	<div class="col-sm-5 us">
+		<p class="text-right">
+			<a href="{$host}" title="{$name}"><i class="glyphicon glyphicon-home"></i> 首页</a> | <a href="#" title="联系我们"><i class="glyphicon glyphicon-envelope"></i> 联系我们</a>
+		</p>
+		<p class="text-right">销售热线：<span class="saleNumber">0513-86528649 18012291278</span>（张先生）</p>
+	</div>
+</div>
 <nav class="navbar navbar-default navbarStyle">
 	<div class="container">
 		<div class="navbar-header">
